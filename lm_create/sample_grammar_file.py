96ae2927abeac0e2a23e969fb6a6bd33f342218a
Roopesh <roopesh.munnaluri1@gmail.com>
2020-12-10 21:54:24 -0500

    created another set of data

--- lm_create/sample_grammar_file.py
+++ lm_create/sample_grammar_file.py
@@ -1,4 +1,4 @@
-names=open("transcripts.txt",'r')
+names=open("sample1/sample_test.txt",'r')
 updatedNames=open("name2.txt",'a')
 for name in names:
   updatedNames.write('<s> ' + name.rstrip() + ' </s>' + '\n')
